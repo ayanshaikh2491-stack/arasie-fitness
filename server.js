aec6e8bfc4cd3bd5d51ec16b61c0d9b515dd9280
Ayan Shaikh <ayanshaikh2491@gmail.com>
2026-06-19 23:19:57 +0530

    Add self-ping every 5 min to prevent sleep

--- server.js
+++ server.js
@@ -10,4 +10,11 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-app.listen(PORT, () => console.log(`ARAISE running on :${PORT}`));
+app.listen(PORT, () => {
+  console.log(`ARAISE running on :${PORT}`);
+  // Self-ping every 5 min to prevent Render sleep
+  const SELF_URL = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`;
+  setInterval(() => {
+    fetch(SELF_URL).catch(() => {});
+  }, 5 * 60 * 1000);
+});
